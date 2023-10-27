c0f7aa971b7da1c250eee6bc598c95f3de03f6db
Samson270 <samuelerinchiuso@gmail.com>
2023-10-27 14:17:38 +0200

    aasa

--- src/app/Components/catalogo/catalogo.component.ts
+++ src/app/Components/catalogo/catalogo.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Macchina } from 'src/app/Models/Macchina';
+import { PeriodoNoleggio } from 'src/app/Models/PeriodoNoleggio';
 import { AddCarService } from 'src/app/Services/add-car.service';
+import { BookingService } from 'src/app/Services/booking.service';
 import { LoginService } from 'src/app/Services/login.service';
 
 @Component({
@@ -11,8 +14,21 @@ import { LoginService } from 'src/app/Services/login.service';
 export class CatalogoComponent {
   macchine: Macchina[];
   loggato:boolean;
-  constructor(private addCarService: AddCarService, private loginService:LoginService){}
+  minDate: string;
+  retDate: string;
+
+
+  constructor(private addCarService: AddCarService, private loginService:LoginService, private bookingService: BookingService){
+    const today = new Date(); 
+    this.minDate = today.toISOString().split('T')[0];
+  }
   
+
+  periodoNoleggio = new FormGroup({
+    dataInizio: new FormControl(''),
+    dataFine: new FormControl(''),
+});
+
   ngOnInit(){
     this.addCarService.getAllCars().subscribe((res: Macchina[]) =>{
       this.macchine = res;
@@ -21,4 +37,22 @@ export class CatalogoComponent {
       this.loggato = res;
     })
   }
+
+  cambiaRet(){
+      const dataInizio = this.periodoNoleggio.get('dataInizio').value;
+    if (dataInizio) {
+      const dataTmp = new Date(dataInizio);
+      dataTmp.setDate(dataTmp.getDate() + 3);
+      this.retDate = dataTmp.toISOString().split('T')[0];
+    } else {
+      this.retDate = ''; // Imposta retDate su vuoto se dataInizio non è impostato
+    }
+  }
+  controllaData(){
+    let tmp1 = new Date(this.periodoNoleggio.get('dataInizio').value);
+    let tmp2 = new Date(this.periodoNoleggio.get('dataFine').value);
+    let tmp = new PeriodoNoleggio(tmp1, tmp2);
+    this.bookingService.controllaDisponibilita(tmp);
+  }
+  
 }
